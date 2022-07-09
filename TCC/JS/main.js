139005423e855e731f60a84302a7af148ebc4536
DurvalCavalcante <durvalcavalcante00@gmail.com>
2022-07-08 21:29:37 -0300

    TCC

--- TCC/JS/main.js
+++ TCC/JS/main.js
@@ -12,3 +12,18 @@ const callback = (elements) => {
 
 const observer = new IntersectionObserver(callback);
 cards.forEach((card) => observer.observe(card));
+
+const btn = document.getElementById('b');
+
+function toggleMenu() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+btn.addEventListener('click', toggleMenu);
+
+function ChangeBG() {
+    document.getElementById('btn').classList.contains('alt')
+        ? document.getElementById('btn').classList.remove('alt')
+        : document.getElementById('btn').classList.add('alt');
+}
