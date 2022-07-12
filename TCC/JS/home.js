0c41721673883fead13650e0ebf4bd3208d5dc8a
DurvalCavalcante <durvalcavalcante00@gmail.com>
2022-07-11 21:39:33 -0300

    Alteração

---
+++ TCC/JS/home.js
@@ -0,0 +1,16 @@
+AOS.init();
+
+const btn = document.getElementById('b');
+
+function toggleMenu() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+btn.addEventListener('click', toggleMenu);
+
+function ChangeBG() {
+    document.getElementById('btn').classList.contains('alt')
+        ? document.getElementById('btn').classList.remove('alt')
+        : document.getElementById('btn').classList.add('alt');
+}
